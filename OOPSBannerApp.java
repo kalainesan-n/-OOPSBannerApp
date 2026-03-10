3cd2a867578ad7d3da6478a772edd0e01caa45de
kalainesan-n <kalainesannatarajan@gmail.com>
2026-03-11 00:25:20 +0530

    UC5: Compact array initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,40 +2,20 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String line1 = String.join("   ",
-                " OOO ",
-                " OOO ",
-                " PPPP ",
-                " SSSS ");
+        String[] banner = {
 
-        String line2 = String.join("   ",
-                "O   O",
-                "O   O",
-                "P   P",
-                "S     ");
+            String.join("   ", " ***** ", " ***** ", " ******", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "*      "),
+            String.join("   ", "*     *", "*     *", "****** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*      ", "      *"),
+            String.join("   ", "*     *", "*     *", "*      ", "*     *"),
+            String.join("   ", "*     *", "*     *", "*      ", "*     *"),
+            String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
 
-        String line3 = String.join("   ",
-                "O   O",
-                "O   O",
-                "PPPP ",
-                " SSS  ");
+        };
 
-        String line4 = String.join("   ",
-                "O   O",
-                "O   O",
-                "P     ",
-                "     S");
-
-        String line5 = String.join("   ",
-                " OOO ",
-                " OOO ",
-                "P     ",
-                " SSSS ");
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
