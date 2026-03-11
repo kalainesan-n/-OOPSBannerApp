d645a56b938fa9bc052301066ba3dd5031252993
kalainesan-n <kalainesannatarajan@gmail.com>
2026-03-11 09:13:23 +0530

    UC2: Print Banner OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,63 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * OOPS Banner Application
+ * @author Kalainesan
+ * @version 2.0
+ */
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-
-        // Create Map to store character patterns
-        Map<Character, String[]> patternMap = new HashMap<>();
-
-        // Pattern for O
-        patternMap.put('O', new String[]{
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        });
-
-        // Pattern for P
-        patternMap.put('P', new String[]{
-                "****** ",
-                "*     *",
-                "****** ",
-                "*      ",
-                "*      ",
-                "*      ",
-                "*      "
-        });
-
-        // Pattern for S
-        patternMap.put('S', new String[]{
-                " ***** ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "*     *",
-                "*     *",
-                " ***** "
-        });
-
-        // Word to print
-        String word = "OOPS";
-
-        // Banner height
-        int height = 7;
-
-        // Print banner
-        for (int row = 0; row < height; row++) {
-
-            for (char letter : word.toCharArray()) {
-
-                String[] pattern = patternMap.get(letter);
-
-                System.out.print(pattern[row] + "   ");
-            }
-
-            System.out.println();
-        }
+        System.out.println(" ***   ***  ***  ***");
+        System.out.println("*   * *   * *  * *  ");
+        System.out.println("*   * *   * ***  *** ");
+        System.out.println("*   * *   * *      *");
+        System.out.println("*   * *   * *      *");
+        System.out.println("*   * *   * *  * *  ");
+        System.out.println(" ***   ***  *    ***");
     }
 }
