49caf3766a882552f9553cda64aa0f638a283218
kalainesan-n <kalainesannatarajan@gmail.com>
2026-03-11 09:24:12 +0530

    UC4: Use array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,22 @@
 /**
  * OOPS Banner Application
  * @author Kalainesan
- * @version 2.0
+ * @version 4.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println(" ***   ***  ***  ***");
-        System.out.println("*   * *   * *  * *  ");
-        System.out.println("*   * *   * ***  *** ");
-        System.out.println("*   * *   * *      *");
-        System.out.println("*   * *   * *      *");
-        System.out.println("*   * *   * *  * *  ");
-        System.out.println(" ***   ***  *    ***");
+        String[] banner = {
+            " ***   ***  ***  ***",
+            "*   * *   * *  * *  ",
+            "*   * *   * ***  ** ",
+            "*   * *   * *      *",
+            "*   * *   * *      *",
+            "*   * *   * *  * *  ",
+            " ***   ***  *    ***"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
